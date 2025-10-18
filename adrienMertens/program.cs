09c7ee4df4b42d6db6cdffd75a22c9305f0d90c8
Mertens Adrien <mertensadrien23@hotmail.be>
2025-10-18 23:49:53 +0200

    Update `Person` class with constructor and public properties; add example usage in `Program` class

--- adrienMertens/program.cs
+++ adrienMertens/program.cs
@@ -1,8 +1,15 @@
 ﻿class Person
 {
-    private string FirstName;
-    private string LastName;
-    private DateTime BirthDate;
+    public string FirstName { get;set };
+    public string LastName { get;set };
+    public DateTime BirthDate { get;set };
+
+    public Person(string firstName, string lastName, DateTime birthDate)
+    {
+        firstName = firstName;
+        lastName = lastName;
+        birthDate = birthDate;
+    }
 }
 
 class CurrentAccount
@@ -45,4 +52,12 @@ class Bank
     {
         
     }
+}
+
+class Program
+{
+    // Exemple d'utilisation
+    var bank = new Bank("Ma Banque");
+    var person = new Person("Adrien", "Mertens", new DateTime(1990, 1, 1));
+    var account = new CurrentAccount("BE123456789", person, 1000);
 }
